ab8bae06a029505a67dbb1a32101501e161d1c3e
silvadpablo <teacherpablod@hotmail.com>
2022-10-26 18:55:00 -0300

    create requests

---
+++ scripts/requets.js
@@ -0,0 +1,34 @@
+const baseUrl = "https://m2-api-living.herokuapp.com/news"
+
+export async function getAllPosts () {
+    try {
+        const request = await fetch(`${baseUrl}?page=0`, {
+            method: "GET",
+        })
+        if (request.ok){
+            const allPosts = await request.json()
+            return allPosts.news
+        } else {
+            throw request
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+console.log(await getAllPosts())
+
+export async function getPostById (postId) {
+    try {
+        const request = await fetch(`${baseUrl}/${postId}`,{
+            method: "GET",
+        })
+        if (request.ok){
+            const post = request.json()
+            return post
+        } else {
+            throw request
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
